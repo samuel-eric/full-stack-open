5403abdd653590f139b978c81623d5afff181ed6
samuel-eric <samueltan086@gmail.com>
2023-10-29 21:05:54 +0700

    Add submission for exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,36 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>;
-
-const Total = ({ sum }) => (
-	<p>
-		<strong>Total of {sum} exercises</strong>
-	</p>
-);
-
-const Part = ({ part }) => (
-	<p>
-		{part.name} {part.exercises}
-	</p>
-);
-
-const Content = ({ parts }) => (
-	<>
-		{parts.map((part) => (
-			<Part key={part.id} part={part} />
-		))}
-	</>
-);
-
-const Course = ({ course }) => {
-	return (
-		<>
-			<Header course={course.name} />
-			<Content parts={course.parts} />
-			<Total
-				sum={course.parts.reduce((sum, part) => sum + part.exercises, 0)}
-			/>
-		</>
-	);
-};
+import Course from './Course';
 
 const App = () => {
 	const courses = [
