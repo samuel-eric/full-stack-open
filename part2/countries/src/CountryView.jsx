c40031c2a6cab17561b3f41e6cff9a04232b9d3d
samuel-eric <samueltan086@gmail.com>
2023-11-02 08:47:57 +0700

    Add submission for exercise 2.20

--- part2/countries/src/CountryView.jsx
+++ part2/countries/src/CountryView.jsx
@@ -1,4 +1,31 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const api_key = import.meta.env.VITE_API_KEY;
+
 const CountryView = ({ country }) => {
+	const [weather, setWeather] = useState(null);
+	useEffect(() => {
+		axios
+			.get(
+				`http://api.openweathermap.org/geo/1.0/direct?q=${country.capital[0]}&appid=${api_key}`
+			)
+			.then(({ data }) =>
+				axios.get(
+					`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${api_key}`
+				)
+			)
+			.then(({ data }) =>
+				setWeather({
+					temp: data.main.temp,
+					wind: data.wind.speed,
+					icon: data.weather[0].icon,
+				})
+			);
+	}, [country]);
+
+	const convertKelvinToCelcius = (kelvin) => (kelvin - 273.15).toFixed(2);
+
 	return (
 		<div>
 			<h1>{country.name.common}</h1>
@@ -13,6 +40,19 @@ const CountryView = ({ country }) => {
 				))}
 			</ul>
 			<img src={country.flags.png} alt={country.flags.alt} />
+			<h3>Weather in {country.capital[0]}</h3>
+			{weather ? (
+				<div>
+					<p>temperature {convertKelvinToCelcius(weather.temp)} celcius</p>
+					<img
+						src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+						alt="weather icon"
+					/>
+					<p>wind {weather.wind} m/s</p>
+				</div>
+			) : (
+				<p>Loading weather data...</p>
+			)}
 		</div>
 	);
 };
